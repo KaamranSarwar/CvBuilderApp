f27d08d4def0c9e45dd65a580dec075052757abe
KaamranSarwar <kaamran.sarwar@gmail.com>
2025-03-16 21:13:45 +0500

    Completed Main Screen Front End

--- app/src/main/java/com/example/cvbuilderapp/MainActivity.java
+++ app/src/main/java/com/example/cvbuilderapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.cvbuilderapp;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnProfilePic,btnPersonalDetails,btnEducation,btnSummary,btnReferences,btnCertifications,btnExperience;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,4 +23,16 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+    private void init()
+    {
+        btnProfilePic = findViewById(R.id.btnProfilePic);
+        btnPersonalDetails = findViewById(R.id.btnPersonalDetails);
+        btnEducation = findViewById(R.id.btnEducation);
+        btnSummary = findViewById(R.id.btnSummary);
+        btnCertifications = findViewById(R.id.btnCertifications);
+        btnReferences = findViewById(R.id.btnReferences);
+        btnExperience = findViewById(R.id.btnExperience);
+
+    }
+
 }
